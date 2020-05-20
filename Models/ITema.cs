258c27155396094eb9439a318af386bf96fda42c
Diego Zepeda Torres <diegoztorres01@gmail.com>
2020-05-20 15:52:03 -0500

    Implementacion Inteface (Tema)

---
+++ Models/ITema.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MVCLaboratorio.Models
+{
+    public interface ITema
+    {
+        List<Tema> obtenerTemas();
+        Video obtenerTema(int IdTema);
+        void insertarTema(Tema datosTema);
+        void eliminarTema(int IdTema);
+        void actualizarTema(Tema datosTema);
+    }
+}
